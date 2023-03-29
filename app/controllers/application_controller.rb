ecb4877c5da2c4dd3b9e5a2767a5d06ad0a83666
Eric <eric00471@gmail.com>
2023-03-29 14:43:39 +0800

    CRUD speed test: finish in 19mins

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def record_not_found
+    render file: Rails.root.join('public', '404.html'),
+                  layout: false,
+                  status: 404 and return
+  end 
+end
